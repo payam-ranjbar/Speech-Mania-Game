eb6dcaae48ef7bffd73a258a22a71d011a58d7bb
PM <payam.ranjbar@live.com>
2025-02-27 15:32:55 -0700

    Player avatar spining

--- Assets/Scripts/PlayerAvatar.cs
+++ Assets/Scripts/PlayerAvatar.cs
@@ -1,13 +1,38 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 
 public class PlayerAvatar: MonoBehaviour
 {
         [SerializeField] private string stateName;
-        
+        [SerializeField] private GameObject mesh;
         public string StateName => stateName;
+
+        private Coroutine _coroutine;
+        private void OnEnable()
+        {
+                if(_coroutine != null) return;
+
+                _coroutine = StartCoroutine(RotateContinuously());
+        }
+
+        private void OnDisable()
+        {
+                if(_coroutine is null) return;
+                StopCoroutine(_coroutine);
+        }
 
         public void SetState(PlayerState playerState)
         {
                 stateName = playerState.stateName;
+        }
+        
+        IEnumerator RotateContinuously()
+        {
+                while (true) // Infinite loop
+                {
+                        transform.Rotate(0, 100 * Time.deltaTime, 0);
+                        yield return null; // Wait until the next frame
+                }
         }
 }
